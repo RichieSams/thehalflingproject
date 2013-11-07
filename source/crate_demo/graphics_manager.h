0d4ac7454d4843880cc71f639725fe5766c7aabd
RichieSams <adastley@gmail.com>
2013-11-06 22:23:40 -0600

    CRATE_DEMO: Remove all logic that is now in GraphicsManagerBase

--- source/crate_demo/graphics_manager.h
+++ source/crate_demo/graphics_manager.h
@@ -7,34 +7,24 @@
 #ifndef CRATE_DEMO_GRAPHICS_MANAGER_H
 #define CRATE_DEMO_GRAPHICS_MANAGER_H
 
-#include "common/graphics_manager_interface.h"
+#include "common/graphics_manager_base.h"
 
 #include <d3d11.h>
 
 namespace CrateDemo {
 
-class GraphicsManager : public Common::IGraphicsManager {
+class GameStateManager;
+class GraphicsManager : public Common::GraphicsManagerBase {
 public:
-	GraphicsManager();
+	GraphicsManager(GameStateManager *gameStateManager);
 
 private:
-	ID3D11Device *m_d3dDevice;
-	ID3D11DeviceContext *m_d3dImmediateContext;
-	IDXGISwapChain *m_swapChain;
-	ID3D11Texture2D *m_depthStencilBuffer;
+	GameStateManager *m_gameStateManager;
+
 	ID3D11RenderTargetView *m_renderTargetView;
-	ID3D11DepthStencilView *m_depthStencilView;
-	D3D11_VIEWPORT m_screenViewport;
 
-	// Screen dimensions
-	int m_clientWidth;
-	int m_clientHeight;
 
-	// MSAA
-	bool m_enable4xMSAA;
-	uint m_4xMSAAQuality;
 
-	bool m_d3dInitialized;
 
 public:
 	bool Initialize(int clientWidth, int clientHeight, HWND hwnd);

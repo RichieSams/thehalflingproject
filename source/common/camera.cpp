9f289dff3440ef405e29b49123acf5dc208f6926
RichieSams <adastley@gmail.com>
2013-12-18 20:21:45 -0600

    COMMON: Create a camera class to handle all "eye" movement and matrix calculations

---
+++ source/common/camera.cpp
@@ -0,0 +1,36 @@
+/* The Halfling Project - A Graphics Engine and Projects
+ *
+ * The Halfling Project is the legal property of Adrian Astley
+ * Copyright Adrian Astley 2013
+ */
+
+#include "common/camera.h"
+
+
+namespace Common {
+
+void Camera::MoveCamera(float dTheta, float dPhi, float dRadius) {
+	if (m_up > 0.0f)
+		m_theta += dTheta;
+	else
+		m_theta -= dTheta;
+
+	m_phi += dPhi;
+	m_radius += dRadius;
+
+	// Keep phi within -2PI to +2PI for easy 'up' comparison
+	if (m_phi > DirectX::XM_2PI) {
+		m_phi -= DirectX::XM_2PI;
+	} else if (m_phi < -DirectX::XM_2PI) {
+		m_phi += DirectX::XM_2PI;
+	}
+
+	// If phi is between 0 to PI or -PI to -2PI, make 'up' be positive Y, other wise make it negative Y
+	if ((m_phi > 0 && m_phi < DirectX::XM_PI) || (m_phi < -DirectX::XM_PI && m_phi > -DirectX::XM_2PI)) {
+		m_up = 1;
+	} else {
+		m_up = -1;
+	}
+}
+
+} // End of namespace Common
---
+++ source/common/camera.h
@@ -0,0 +1,48 @@
+/* The Halfling Project - A Graphics Engine and Projects
+ *
+ * The Halfling Project is the legal property of Adrian Astley
+ * Copyright Adrian Astley 2013
+ */
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "DirectXMath.h"
+
+
+namespace Common {
+
+class Camera {
+public:
+	Camera() : m_theta(0.0f), m_phi(0.0f), m_radius(0.0f), m_up(1.0f) {}
+	Camera(float theta, float phi, float radius) : m_theta(theta), m_phi(phi), m_radius(radius), m_up(1.0f) {}
+
+private:
+	float m_theta;
+	float m_phi;
+	float m_radius;
+	float m_up;
+
+public:
+	void MoveCamera(float dTheta, float dPhi, float dRadius);
+	inline DirectX::XMMATRIX Camera::CreateViewMatrix(DirectX::XMVECTOR target) const {
+		DirectX::XMVECTOR pos = ToCartesianVector();
+		DirectX::XMVECTOR up = DirectX::XMVectorSet(0.0f, m_up, 0.0f, 0.0f);
+
+		return DirectX::XMMatrixLookAtLH(pos, target, up);
+	}
+
+private:
+	inline DirectX::XMVECTOR Camera::ToCartesianVector() const {
+		float x = m_radius * sinf(m_phi) * sinf(m_theta);
+		float y = m_radius * cosf(m_phi);
+		float z = m_radius * sinf(m_phi) * cosf(m_theta);
+		float w = 1.0f;
+
+		return DirectX::XMVectorSet(x, y, z, w);
+	}
+};
+
+} // End of namespace Common
+
+#endif

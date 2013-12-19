54b3392b6b38063e2b9aea3465515a4a848bc3c0
RichieSams <adastley@gmail.com>
2013-12-18 20:22:50 -0600

    COMMON: Create a class for holding int vectors of various sizes
    Float vectors can use DirectX Math structs

---
+++ source/common/vector.h
@@ -0,0 +1,36 @@
+/* The Halfling Project - A Graphics Engine and Projects
+ *
+ * The Halfling Project is the legal property of Adrian Astley
+ * Copyright Adrian Astley 2013
+ */
+
+#ifndef VECTOR_H
+#define VECTOR_H
+
+
+namespace Common {
+
+struct Vector2 {
+public:
+	int x;
+	int y;
+};
+
+struct Vector3 {
+public:
+	int x;
+	int y;
+	int z;
+};
+
+struct Vector4 {
+public:
+	int x;
+	int y;
+	int z;
+	int w;
+};
+
+} // End of namespace Common
+
+#endif

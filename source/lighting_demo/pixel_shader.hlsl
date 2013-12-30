8b3fb601cb6008c56d7c039851c6e96e49650095
RichieSams <adastley@gmail.com>
2013-12-30 11:52:57 -0600

    LIGHTING_DEMO: Update pixel shader for lighting

--- source/lighting_demo/pixel_shader.hlsl
+++ source/lighting_demo/pixel_shader.hlsl
@@ -5,7 +5,56 @@
  */
 
 #include "types.hlsli"
+#include "common/shaders/materials.hlsli"
+#include "common/shaders/lights.hlsli"
+#include "common/shaders/light_functions.hlsli"
 
-float4 PS(VertexOut pin) : SV_TARGET {
-	return pin.color;
+
+cbuffer cbPerObject : register(b2) {
+	Material gMaterial;
+};
+
+StructuredBuffer<DirectionalLight> gDirectionalLights : register(t4);
+StructuredBuffer<PointLight> gPointLights : register(t5);
+StructuredBuffer<SpotLight> gSpotLights : register(t6);
+
+
+float4 PS(PixelIn input) : SV_TARGET {
+	// Interpolating can unnormalize
+	input.normalView = normalize(input.normalView);
+	input.positionView = normalize(input.positionView);
+
+	// Initialize
+	float4 ambient = float4(0.0f, 0.0f, 0.0f, 0.0f);
+	float4 diffuse = float4(0.0f, 0.0f, 0.0f, 0.0f);
+	float4 spec = float4(0.0f, 0.0f, 0.0f, 0.0f);
+
+	// Sum the contribution from each source
+
+	uint numLights, dummy, lightIndex;
+
+	gDirectionalLights.GetDimensions(numLights, dummy);
+	for (lightIndex = 0; lightIndex < numLights; ++lightIndex) {
+        DirectionalLight light = gDirectionalLights[lightIndex];
+		AccumulateDirectionalLight(gMaterial, light, input.positionView, input.normalView, ambient, diffuse, spec);
+    }
+
+	gPointLights.GetDimensions(numLights, dummy);
+	for (lightIndex = 0; lightIndex < numLights; ++lightIndex) {
+        PointLight light = gPointLights[lightIndex];
+		AccumulatePointLight(gMaterial, light, input.positionView, input.normalView, ambient, diffuse, spec);
+    }
+
+	gSpotLights.GetDimensions(numLights, dummy);
+	for (lightIndex = 0; lightIndex < numLights; ++lightIndex) {
+        SpotLight light = gSpotLights[lightIndex];
+		AccumulateSpotLight(gMaterial, light, input.positionView, input.normalView, ambient, diffuse, spec);
+    }
+
+	float4 litColor = ambient + diffuse + spec;
+
+	// Take alpha from diffuse material
+	litColor.a = gMaterial.Diffuse.a;
+
+	return litColor;
 }

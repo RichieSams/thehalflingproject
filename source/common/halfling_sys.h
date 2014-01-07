a9fe5d311909b20ea275fb985a7b12eb794180e6
RichieSams <adastley@gmail.com>
2014-01-06 23:40:43 -0600

    COMMON: Create define for asserting with a message

--- source/common/halfling_sys.h
+++ source/common/halfling_sys.h
@@ -25,4 +25,15 @@
 #ifdef max
 	#undef max
 #endif
+
+#define AssertMsg(condition, message) \
+	do {                                                                        \
+		if (!(condition)) {                                                     \
+			std::cerr << "Assertion `" #condition "` failed in " << __FILE__    \
+			<< " line " << __LINE__ << ": " << message << std::endl;            \
+			std::exit(EXIT_FAILURE);                                            \
+		}                                                                       \
+	} while (false)
+
+
 #endif

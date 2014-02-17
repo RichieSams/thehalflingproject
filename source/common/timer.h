2575a1cc035dc8fa10163240c41f7a2e000a1bb6
RichieSams <adastley@gmail.com>
2014-02-17 15:38:40 -0600

    COMMON: Create a simple timer/stopwatch class
    Useful for naive profiling

---
+++ source/common/timer.h
@@ -0,0 +1,35 @@
+/* The Halfling Project - A Graphics Engine and Projects
+ *
+ * The Halfling Project is the legal property of Adrian Astley
+ * Copyright Adrian Astley 2013
+ */
+
+#ifndef COMMON_TIMER_H
+#define COMMON_TIMER_H
+
+#include "common/typedefs.h"
+
+
+namespace Common {
+
+class Timer {
+public:
+	Timer(int64 performanceCounterFreq = 0);
+
+private:
+	double m_milliSecondsPerCount;
+
+	int64 m_startCount;
+	int64 m_accumulatedTicks;
+
+	bool m_isRunning;
+
+public:
+	void Start();
+	void Stop();
+	double GetTime();
+};
+
+} // End of namespace Common
+
+#endif

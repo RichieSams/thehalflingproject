4188ec9babcf264b6a151dcc7de47b3e3c7ce7d8
RichieSams <adastley@gmail.com>
2014-04-13 00:23:16 -0500

    LIBS: Force the use of CreateFileW instead of CreateFile2
    CreateFile2 is only for win8

--- source/libs/DirectXTex/DDSTextureLoader.cpp
+++ source/libs/DirectXTex/DDSTextureLoader.cpp
@@ -154,13 +154,6 @@ static HRESULT LoadTextureDataFromFile( _In_z_ const wchar_t* fileName,
     }
 
     // open the file
-#if (_WIN32_WINNT >= _WIN32_WINNT_WIN8)
-    ScopedHandle hFile( safe_handle( CreateFile2( fileName,
-                                                  GENERIC_READ,
-                                                  FILE_SHARE_READ,
-                                                  OPEN_EXISTING,
-                                                  nullptr ) ) );
-#else
     ScopedHandle hFile( safe_handle( CreateFileW( fileName,
                                                   GENERIC_READ,
                                                   FILE_SHARE_READ,
@@ -168,7 +161,6 @@ static HRESULT LoadTextureDataFromFile( _In_z_ const wchar_t* fileName,
                                                   OPEN_EXISTING,
                                                   FILE_ATTRIBUTE_NORMAL,
                                                   nullptr ) ) );
-#endif
 
     if ( !hFile )
     {

f228d5a837fa16ab34ea8123a595f0226369eab5
RichieSams <adastley@gmail.com>
2014-05-15 17:25:37 -0500

    COMMON: Create a class for caching models
    This reduces the memory load if an artist didn't explicitly using instancing,
    but rather, just put a couple copies of the model into the scene.

---
+++ source/common/model_manager.cpp
@@ -0,0 +1,34 @@
+/* The Halfling Project - A Graphics Engine and Projects
+ *
+ * The Halfling Project is the legal property of Adrian Astley
+ * Copyright Adrian Astley 2013 - 2014
+ */
+
+#include "common/model_manager.h"
+
+#include "common/halfling_model_file.h"
+
+
+namespace Common {
+
+ModelManager::~ModelManager() {
+	for (auto iter = m_modelCache.begin(); iter != m_modelCache.end(); ++iter) {
+		delete iter->second;
+	}
+}
+
+Model *ModelManager::GetModel(ID3D11Device *device, Common::TextureManager *textureManager, std::wstring filePath) {
+	// First check the cache
+	auto iter = m_modelCache.find(filePath);
+	if (iter != m_modelCache.end()) {
+		return iter->second;
+	}
+
+	// Else create it from scratch
+	Model *newModel = Common::HalflingModelFile::Load(device, textureManager, filePath.c_str());
+	m_modelCache[filePath] = newModel;
+
+	return newModel;
+}
+
+} // End of namespace Common
---
+++ source/common/model_manager.h
@@ -0,0 +1,32 @@
+/* The Halfling Project - A Graphics Engine and Projects
+ *
+ * The Halfling Project is the legal property of Adrian Astley
+ * Copyright Adrian Astley 2013 - 2014
+ */
+
+#ifndef COMMON_MODEL_MANAGER_H
+#define COMMON_MODEL_MANAGER_H
+
+#include "common/model.h"
+
+#include "common/texture_manager.h"
+
+#include <unordered_map>
+
+
+namespace Common {
+
+class ModelManager {
+public:
+	~ModelManager();
+
+private:
+	std::unordered_map<std::wstring, Model *> m_modelCache;
+
+public:
+	Model *GetModel(ID3D11Device *device, Common::TextureManager *textureManager, std::wstring filePath);
+};
+
+} // End of namespace Common
+
+#endif

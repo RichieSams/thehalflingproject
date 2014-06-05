b73131777128780f2817f03f54c20188c917f8ff
RichieSams <adastley@gmail.com>
2014-06-05 02:59:39 -0500

    OBJ_LOADER_DEMO: Output details about COM memory leaks when in debug build

--- source/obj_loader_demo/main.cpp
+++ source/obj_loader_demo/main.cpp
@@ -8,15 +8,33 @@
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	ObjLoaderDemo::ObjLoaderDemo app(hInstance);
-	DBG_UNREFERENCED_PARAMETER(hPrevInstance);
-	DBG_UNREFERENCED_PARAMETER(lpCmdLine);
-	DBG_UNREFERENCED_PARAMETER(nCmdShow);
+	#ifdef _DEBUG
+	ID3D11Debug *debugInterface = nullptr;
+	#endif
+	
+	// Force scope so we can guarantee the destructor is called before we try to use ReportLiveDeviceObjects
+	{
+		ObjLoaderDemo::ObjLoaderDemo app(hInstance);
+		DBG_UNREFERENCED_PARAMETER(hPrevInstance);
+		DBG_UNREFERENCED_PARAMETER(lpCmdLine);
+		DBG_UNREFERENCED_PARAMETER(nCmdShow);
 
-	app.Initialize(L"OBJ Loader Demo", 1280, 720, false);
-	app.Run();
+		app.Initialize(L"OBJ Loader Demo", 1280, 720, false);
 
-	app.Shutdown();
+		#ifdef _DEBUG
+		app.CreateDebugInterface(&debugInterface);
+		#endif
+
+		app.Run();
+
+		app.Shutdown();
+	}
+
+	#ifdef _DEBUG
+	debugInterface->ReportLiveDeviceObjects(D3D11_RLDO_DETAIL);
+
+	ReleaseCOM(debugInterface);
+	#endif
 
 	return 0;
 }

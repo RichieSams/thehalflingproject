a7c235d70abdc375c5d3b934d611cafd237ddf7c
RichieSams <adastley@gmail.com>
2015-01-20 23:30:03 -0600

    COMMON: Create a simple page-based linear allocator

---
+++ source/common/linear_allocator.cpp
@@ -0,0 +1,28 @@
+/* The Halfling Project - A Graphics Engine and Projects
+ *
+ * The Halfling Project is the legal property of Adrian Astley
+ * Copyright Adrian Astley 2013 - 2014
+ */
+
+#include "common/linear_allocator.h"
+
+
+namespace Common {
+
+void *LinearAllocator::Allocate(size_t size) {
+    if (m_current + size >= m_end) {
+        // Allocate a new page;
+        Page *newPage = new Page(m_pageSize);
+        
+        m_currentPage->NextPage = newPage;
+        m_current = m_start = reinterpret_cast<byte *>(newPage->Data);
+        m_end = m_start + m_pageSize;
+    }
+    
+    void* userPtr = m_current;
+    m_current += size;
+    
+    return userPtr;
+}
+
+} // End of namespace Common
---
+++ source/common/linear_allocator.h
@@ -0,0 +1,46 @@
+/* The Halfling Project - A Graphics Engine and Projects
+ *
+ * The Halfling Project is the legal property of Adrian Astley
+ * Copyright Adrian Astley 2013 - 2014
+ */
+
+#pragma once
+
+#include "common/typedefs.h"
+
+
+namespace Common {
+
+class LinearAllocator {
+public:
+    LinearAllocator(size_t pageSize)
+            : m_pageSize(pageSize) {
+        m_currentPage = m_firstPage = new Page(pageSize);
+    }
+
+private:
+    struct Page {
+        Page(size_t pageSize)
+            : NextPage(nullptr),
+              Data(::operator new(pageSize)) {
+        }
+    
+        Page *NextPage;
+        void *Data;
+    };
+
+    size_t m_pageSize;
+    
+    Page *m_firstPage;
+    Page *m_currentPage;
+    
+    byte *m_start;
+    byte *m_end;
+    byte *m_current;
+    
+public:
+    void *Allocate(size_t size);
+    inline void Reset() { m_current = m_start; }
+};
+
+} // End of namespace Common

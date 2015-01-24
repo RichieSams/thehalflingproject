27a120554a9ce97a6bfc521f1cf7ca4ff21a0c24
RichieSams <adastley@gmail.com>
2015-01-24 15:25:17 -0600

    GRAPHICS: Add a GraphicsState constructor

--- source/graphics/graphics_state.h
+++ source/graphics/graphics_state.h
@@ -14,6 +14,23 @@
 namespace Graphics {
 
 struct GraphicsState {
+	GraphicsState()
+			: MaterialShader(nullptr),
+			  IndexBuffer(nullptr),
+			  BlendState(BlendState::BLEND_DISABLED),
+			  SampleMask(0xFFFFFFFF),
+			  RasterizerState(RasterizerState::CULL_BACKFACES),
+			  DepthStencilState(DepthStencilState::REVERSE_DEPTH_WRITE_ENABLED) {
+		// WORKAROUND: We have to manually initialize because VS 2013 compiler doesn't support array initialization in the class initializer list
+		VertexBuffers[0] = nullptr;
+		VertexBuffers[1] = nullptr;
+
+		BlendFactor[0] = 1.0f;
+		BlendFactor[1] = 1.0f;
+		BlendFactor[2] = 1.0f;
+		BlendFactor[3] = 1.0f;
+	}
+
 	MaterialShader *MaterialShader;
 	ID3D11Buffer *VertexBuffers[2];
 	ID3D11Buffer *IndexBuffer;

02ea40c4f10764aca3ac1489f02bd8728a5dab93
RichieSams <adastley@gmail.com>
2015-02-21 18:10:59 -0600

    LIB_DOWNLOADER: Create a c# application to download all the required libs needed
    So the end user (and the CI compiler) can easily be set up and compiling.

---
+++ source/lib_downloader/Downloader.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace LibDownloader {
+	partial class Downloader {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.m_progressBar = new System.Windows.Forms.ProgressBar();
+			this.m_console = new System.Windows.Forms.RichTextBox();
+			this.m_progressLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// m_progressBar
+			// 
+			this.m_progressBar.Location = new System.Drawing.Point(12, 160);
+			this.m_progressBar.Name = "m_progressBar";
+			this.m_progressBar.Size = new System.Drawing.Size(440, 30);
+			this.m_progressBar.TabIndex = 0;
+			// 
+			// m_console
+			// 
+			this.m_console.Location = new System.Drawing.Point(12, 12);
+			this.m_console.Name = "m_console";
+			this.m_console.ReadOnly = true;
+			this.m_console.Size = new System.Drawing.Size(440, 142);
+			this.m_console.TabIndex = 1;
+			this.m_console.Text = "";
+			// 
+			// m_progressLabel
+			// 
+			this.m_progressLabel.AutoSize = true;
+			this.m_progressLabel.Location = new System.Drawing.Point(220, 169);
+			this.m_progressLabel.Name = "m_progressLabel";
+			this.m_progressLabel.Size = new System.Drawing.Size(24, 13);
+			this.m_progressLabel.TabIndex = 2;
+			this.m_progressLabel.Text = "0 %";
+			// 
+			// Downloader
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(464, 202);
+			this.Controls.Add(this.m_progressLabel);
+			this.Controls.Add(this.m_console);
+			this.Controls.Add(this.m_progressBar);
+			this.Name = "Downloader";
+			this.Text = "The Halfling Project - Required Libs Downloader";
+			this.Shown += new System.EventHandler(this.OnShown);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ProgressBar m_progressBar;
+		private System.Windows.Forms.RichTextBox m_console;
+		private System.Windows.Forms.Label m_progressLabel;
+	}
+}
---
+++ source/lib_downloader/Downloader.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows.Forms;
+
+namespace LibDownloader {
+	public partial class Downloader : Form {
+		public Downloader() {
+			InitializeComponent();
+
+			m_webClient = new WebClient();
+			m_webClient.DownloadProgressChanged += ProgressChanged;
+			m_webClient.DownloadFileCompleted += Completed;
+			m_webClient.Proxy = null;
+
+			m_stopwatch = new Stopwatch();
+
+			m_currentDownload = null;
+		}
+
+		private WebClient m_webClient;
+		private Stopwatch m_stopwatch;
+
+		private Queue<Tuple<Uri, string>> m_filesToDownload;
+		private Tuple<Uri, string> m_currentDownload;
+		private int m_fileSize;
+
+		private void OnShown(object sender, EventArgs e) {
+			// Read the files to download
+			ReadBinaryLocations();
+
+			// Start the first download
+			Completed(null, null);
+		}
+
+		private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e) {
+			var percentage = (int)((float)e.BytesReceived / m_fileSize * 100);
+
+			// Update the download label
+			m_progressLabel.Text = string.Format("{0}%", percentage);
+
+			// Update the progressbar percentage only when the value is not the same.
+			m_progressBar.Value = percentage;
+		}
+
+		private void Completed(object sender, AsyncCompletedEventArgs e) {
+			m_stopwatch.Stop();
+			m_stopwatch.Reset();
+
+			
+			if (m_currentDownload != null) {
+				// Decompress the files
+				m_console.Text += "Extracting: " + m_currentDownload.Item2 + "\n";
+				m_console.ScrollToCaret();
+				using (var archive = ZipFile.OpenRead(m_currentDownload.Item2)) {
+					foreach (var file in archive.Entries) {
+						var completeFileName = Path.Combine("libs", file.FullName);
+
+						// Assuming Empty for Directory
+						if (file.Name == "") {
+							Directory.CreateDirectory(Path.GetDirectoryName(completeFileName));
+							continue;
+						}
+
+						file.ExtractToFile(completeFileName, true);
+					}
+				}
+
+				// Delete the archive
+				File.Delete(m_currentDownload.Item2);
+			}
+
+			if (m_filesToDownload.Count == 0) {
+				m_console.Text += "Done\n";
+				m_console.ScrollToCaret();
+				return;
+			}
+
+			m_currentDownload = m_filesToDownload.Dequeue();
+
+			var req = WebRequest.Create(m_currentDownload.Item1);
+			req.Proxy = null;
+			req.Method = "HEAD";
+			using (var resp = req.GetResponse()) {
+				m_fileSize = int.Parse(resp.Headers.Get("Content-Length"));
+			}
+
+			m_console.Text += "Downloading: " + m_currentDownload.Item2 + "\n";
+			m_console.ScrollToCaret();
+			m_stopwatch.Start();
+
+			m_webClient.DownloadFileAsync(m_currentDownload.Item1, m_currentDownload.Item2);
+		}
+
+		private void ReadBinaryLocations() {
+			m_filesToDownload = new Queue<Tuple<Uri, string>>();
+
+			using (var reader = new StreamReader(new FileStream("libs\\binary_download_locations.txt", FileMode.Open))) {
+				string line;
+
+				while ((line = reader.ReadLine()) != null) {
+					var splitLine = line.Split(new[] {"=>", " "}, StringSplitOptions.RemoveEmptyEntries);
+
+					m_filesToDownload.Enqueue(new Tuple<Uri, string>(new Uri(splitLine[0]), splitLine[1]));
+				}
+			}
+		}
+
+	}
+}
